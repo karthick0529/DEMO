9f275fa2caebf671d545f7997532e83a584a6adb
KARTHICK-R <karthicknandha619@gmail.com>
2024-07-29 14:50:09 +0530

    code for Login component and updating App.jsx

--- FSD-DEMO/client/src/App.jsx
+++ FSD-DEMO/client/src/App.jsx
@@ -1,13 +1,30 @@
-import React, { useState } from 'react';
-import {BrowserRouter as router} from "react-router-dom";
-import jwtdecode from 'jwt-decode';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import jwtDecode from "jwt-decode";
+import "./App.css";
+import Navbar from "../components/navbar/Navbar";
+import Routing from "../components/routing/Routing";
+import setAuthToken from "../utils/setAuthToken";
 
+let logUser;
+if (localStorage.token) {
+  const jwt = localstorage.getItem("token");
+  setAuthToken(jwt);
+  logUser = jwtDecode(jwt);
+}
 function App() {
-  
+  const [user, setUser] = useState(logUser);
+  console.log(user);
   return (
-    <div></div>
-  )
+    <Router>
+      <div className="app">
+        <Navbar user={user} />
+          <div className="main">
+            <Routing user={user} />
+          </div>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
