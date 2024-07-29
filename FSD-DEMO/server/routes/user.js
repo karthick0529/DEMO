6496ff09e08addcebf98a9bd59c7ca5f94173148
KARTHICK-R <karthicknandha619@gmail.com>
2024-07-29 12:06:13 +0530

    resister the new user and generating the token

--- FSD-DEMO/server/routes/user.js
+++ FSD-DEMO/server/routes/user.js
@@ -2,10 +2,32 @@ const express = require("express");
 const router = express.Router();
 const User = require("./models/User");
 const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth");
 
-// Current user info
+// Current user information
 
-router.get("/", async(req,res) => {
-    const profile = await User.findById(req.user.id);
-    res.send(profile);
+router.get("/", auth, async (req, res) => {
+  const profile = await User.findById(req.user.id);
+  res.send(profile);
 });
+
+// Register new user
+
+router.post("/", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  //check if user already exists
+  let user = await User.findOne({ email });
+  if (user) return res.status(400).send("User already exists");
+
+  //Save the new user
+  user = new User({ name, email, password });
+  await user.save();
+
+  //Generate Token and send it
+  const jwtData = { id: user_id, name: user.name };
+  const token = jwt.sign(jwtData, process.env.JWTSECRET, { expires: "24hr" });
+  res.send({ token });
+});
+
+module.exports = router;
