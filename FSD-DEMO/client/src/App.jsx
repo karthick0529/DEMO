af6a1ed720ab598643e69db5d787b849c3fa9e2a
KARTHICK-R <karthicknandha619@gmail.com>
2024-07-30 10:47:11 +0530

    first day demo completed.

--- FSD-DEMO/client/src/App.jsx
+++ FSD-DEMO/client/src/App.jsx
@@ -1,21 +1,30 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'
-import { jwtDecode } from 'jwt-decode';
+import { BrowserRouter as Router } from 'react-router-dom';
+import {jwtDecode} from 'jwt-decode';
 
-import './App.css'
-import Navbar from '../src/components/navbar/Navbar';
-import Routing from '../src/components/routing/Routing';
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Routing from './components/routing/Routing';
 import setAuthToken from '../utils/setAuthToken';
 
-let logUser;
-if (localStorage.token) {
-  const jwt = localStorage.getItem('token');
-  setAuthToken(jwt);
-  logUser = jwtDecode(jwt);
+let logUser = null;
+
+if (localStorage.getItem('token')) {
+  try {
+    const jwt = localStorage.getItem('token'); // Get the token as a string
+    setAuthToken(jwt);
+    logUser = jwtDecode(jwt);
+  } catch (error) {
+    console.error("Invalid token:", error);
+    // Remove the invalid token from local storage
+    localStorage.removeItem('token');
+  }
 }
+
 function App() {
   const [user, setUser] = useState(logUser);
   console.log(user);
+  
   return (
     <Router>
       <div className='app'>
@@ -25,6 +34,7 @@ function App() {
         </div>
       </div>
     </Router>
-  )
+  );
 }
-export default App
+
+export default App;
