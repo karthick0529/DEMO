b9469f8db4547e19e7c94eaa7bb0309e5bd5d741
Karthick0529 <karthicknandha619@gmail.com>
2024-09-04 17:37:03 +0530

    updated:

--- URL-Shortner/server/routes/urlRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createShortUrl, redirectToLongUrl, getAllUrls } = require('../controllers/urlController');
-const router = express.Router();
-
-router.post('/', createShortUrl);
-router.get('/:shortUrl', redirectToLongUrl);
-router.get('/', getAllUrls);
-
-module.exports = router;
